cc5b8c29d5c5be746945736c3cd0d2dcfc873e20
Ramona Stefan <ramonastefan365@gmail.com>
2020-03-14 11:23:13 +0200

    reconfigure classes

--- src/main/java/model/Localitate.java
+++ src/main/java/model/Localitate.java
@@ -1,13 +1,16 @@
 package model;
 
+import java.awt.*;
 import java.util.Objects;
 
 public class Localitate extends Regiune {
     private String numeLocalitate;
+    List sport;
 
-    public Localitate(String numeTara, String numeRegiune, String numeLocalitate) {
+    public Localitate(String numeTara, String numeRegiune, String numeLocalitate, List sport) {
         super(numeTara, numeRegiune);
         this.numeLocalitate = numeLocalitate;
+        this.sport = sport;
     }
 
     public String getNumeLocalitate() {
@@ -18,24 +21,34 @@ public class Localitate extends Regiune {
         this.numeLocalitate = numeLocalitate;
     }
 
+    public List getSport() {
+        return sport;
+    }
+
+    public void setSport(List sport) {
+        this.sport = sport;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Localitate that = (Localitate) o;
-        return Objects.equals(numeLocalitate, that.numeLocalitate);
+        return Objects.equals(numeLocalitate, that.numeLocalitate) &&
+                Objects.equals(sport, that.sport);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), numeLocalitate);
+        return Objects.hash(super.hashCode(), numeLocalitate, sport);
     }
 
     @Override
     public String toString() {
         return "Localitate{" +
                 "numeLocalitate='" + numeLocalitate + '\'' +
+                ", sport=" + sport +
                 '}';
     }
 }
